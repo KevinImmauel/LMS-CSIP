0a706e89b4865561035911b52d346d991c74cf42
Kevin Immanuel <kevin.exe4real@gmail.com>
2022-12-18 08:52:53 +0530

    nice

--- libraryfunc.py
+++ libraryfunc.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import main
+from mysql.connector.errors import Error
 
 db = mysql.connector.connect(user='root',password='root',host='localhost')
 
@@ -10,42 +12,127 @@ cur.execute('select * from lib')
 data = cur.fetchall()
 lenofdata = len(data)
 
+def checkbook(book):
+    names=[]
+    for x in data:
+        names.append(x[1])
+    if book in names:
+        return True
+    else:
+        False
+
+def checkauthor(author):
+    authors=[]
+    for x in data:
+        authors.append(x[2])
+    if author in authors:
+        return True
+    else:
+        return False
+
+def checksno(sno):
+    if sno >= len(data):
+        return False
+    else:
+        return True
+
 def insertbook():
-    name = input('Enter the name of the book: ')
-    author = input( 'Enter the name of the author: ')
-    price = int(input('Enter the price of the book: '))
-    pages = int(input('Enter the number of pages ini the book: '))
+    while True:
+        try:
+            name = input('Enter the name of the book: ')
+            author = input( 'Enter the name of the author: ')
+            price = int(input('Enter the price of the book: '))
+            pages = int(input('Enter the number of pages ini the book: '))
+            break
+        except ValueError:
+            print('Value Error! Please try again!')
     cur.execute('''insert into lib values("%d","%s","%s","%d","%d","%s")'''%(lenofdata+1,name,author,price,pages,'1'))
-    db.commit()
-    print('Book added successfully!')
+    try:
+        db.commit()
+        print('Book added successfully!')
+    except mysql.connector.Error as err:
+            print('ERROR! please try again...')
+            print(err)
+            insertbook()
 
-def deletebook(book):
-    for i in data:
-        if i[1]==book:
-            cur.execute('''delete from table lib where name="%s"'''%(book))
-            db.commit()
-            print('Book deleted successfully!')
+def deletebook():
+    book = input('Enter the name of the book to delete: ')
+    if checkbook(book) == True:
+        for i in data:
+            if i[1]==book:
+                cur.execute('''delete from lib where name="%s"'''%(book))
+                db.commit()
+                print('Book deleted successfully!')
+    else:
+        print('Book not in database! Please try again!')
+        deletebook()
 
-def updatebook(book,prop,newpropval):
-    for i in data:
-        if i[1]==book:
-            cur.execute('''update lib set %s="%s" where name="%s"'''%(prop,newpropval,book))
-            db.commit()
+def updatebook():
+    def inupbook(book,prop,newpropval):
+        for i in data:
+            if i[1]==book:
+                cur.execute('''update lib set %s="%s" where name="%s"'''%(prop,newpropval,book))
+                db.commit()
+    book = input('Enter the name of the book you would like to update: ')
+    if checkbook(book) == True:
+        print('''
+        CHOOSE AN OPTION
+        1) CHANGE NAME OF THE BOOK
+        2) CHANGE AUTHOR OF THE BOOK
+        3) CHANGE NUMBER OF PAGES
+        4) CHANGE PRICE
+        ''')
+        optupbook = int(input('Enter your choice: '))
+        if optupbook==1:
+            newpropval = input('Enter new name of the book: ')
+            inupbook(book,'name',newpropval)
+        elif optupbook==2:
+            newpropval = input('Enter new name of the author: ')
+            inupbook(book,'author',newpropval)
+        elif optupbook==3:
+            newpropval = int(input('Enter new number of pages: '))
+            inupbook(book,'pages',newpropval)
+        elif optupbook==4:
+            while True:
+                try:
+                    newpropval = int(input('Enter new price: '))
+                    inupbook(book,'price',newpropval)
+                except ValueError:
+                    print('Please enter an integer')
+                    updatebook()
+        print('Book updated successfully!')
+    else:
+        print('Book not in database! Please try again')
+        updatebook()
 
-def getbook(book):
-    for i in data:
-        if i[1] == book:
-            print(f'{i[1]}, A book written by {i[2]}, contains {i[4]} pages only for {i[3]}$')
-
-def getbookbyauthor(author):
-    for i in data:
-        if i[2]==author:
-            print(f'{i[1]}, A book written by {i[2]}, contains {i[4]} pages only for {i[3]}$')
-
-def getbooksbyauthor(author):
-    for i in data:
-        if i[2]==author:
-            print()
+def getbook():
+    print('''
+        CHOOSE AN OPTION
+        1) SEARCH BOOK BY NAME
+        2) SEARCH BOOK BY AUTHOR
+        3) GO BACK
+        ''')
+    optgb = int(input('Enter your choice :'))
+    if optgb == 1:
+        book = input('Enter a book name: ')
+        if checkbook(book) == True:
+            for i in data:
+                if i[1] == book:
+                    print(f'Nice {i[0]}')
+        else:
+            print('Book does not exist! Try again!')
+            getbook()
+    elif optgb == 2:
+        author = input('Enter an author name: ')
+        if checkauthor(author) == True:
+            for i in data:
+                if i[2] == author:
+                    print(f'Nice {i[0]}')
+        else:
+            print('Author does not exist! Try again!')
+            getbook()
+    elif optgb == 3:
+        main.libscreen()
 
 def borrowbook(bookno):
     for i in data:
--- main.py
+++ main.py
@@ -12,7 +12,13 @@ def mainscreen():
     3) ABOUT THE LIBRARY MANGEMENT SYSTEM
     4) EXIT
     ''')
-    opt = int(input('Enter you choice: '))
+    while True:
+        try:
+            opt = int(input('Enter you choice: '))
+            break
+        except ValueError:
+            print('Value Error! Please try again')
+            mainscreen()
     if opt==1:
         libscreen()
     elif opt==2:
@@ -30,56 +36,41 @@ def libscreen():
     2) DELETE A BOOK
     3) UPDATE A BOOK
     4) SEARCH A BOOK
-    5) GO BACK TO MAIN SCREEN
+    5) CHECK BOOK AVAILABILITY
+    6) GO BACK TO MAIN SCREEN
     ''')
-    optdb = int(input('Enter your choice: '))
+    while True:
+        try:
+            optdb = int(input('Enter you choice: '))
+            break
+        except ValueError:
+            print('Value Error! Please try again')
+            libscreen()
     if optdb==1:
         libraryfunc.insertbook()
         print('DATABASE UPDATED SUCCESSFULLY')
         libscreen()
     elif optdb==2:
-        book = input('Enter the name of the book to delete: ')
-        libraryfunc.deletebook(book)
+        libraryfunc.deletebook()
         print('DATABASE UPDATED SUCCESSFULLY')
         libscreen()
     elif optdb==3:
-        book = input('Enter the name of the book you would like to update: ')
-        print('''
-        CHOOSE AN OPTION
-        1) CHANGE NAME OF THE BOOK
-        2) CHANGE AUTHOR OF THE BOOK
-        3) CHANGE NUMBER OF PAGES
-        4) CHANGE PRICE
-        ''')
-        optupbook = int(input('Enter your choice: '))
-        if optupbook==1:
-            newpropval = input('Enter new name of the book: ')
-            libraryfunc.updatebook(book,'name',newpropval)
-        elif optupbook==2:
-            newpropval = input('Enter new name of the author: ')
-            libraryfunc.updatebook(book,'author',newpropval)
-        elif optupbook==3:
-            newpropval = int(input('Enter new number of pages: '))
-            libraryfunc.updatebook(book,'pages',newpropval)
-        elif optupbook==4:
-            newpropval = int(input('Enter new price: '))
-            libraryfunc.updatebook(book,'price',newpropval)
+        libraryfunc.updatebook()
+        print('DATABASE UPDATED SUCCESSFULLY')
         libscreen()
     elif optdb==4:
-        print('''
-        CHOOSE AN OPTION
-        1) SEARCH BOOK BY NAME
-        2) SEARCH BOOK BY AUTHOR
-        ''')
-        optserbook = int(input('Enter your choice: '))
-        if optserbook==1:
-            book = input('Enter the name of the book: ')
-            libraryfunc.getbook(book)
-        elif optserbook==2:
-            author = input('Enter the name of the author: ')
-            libraryfunc.getbookbyauthor(author)
+        libraryfunc.getbook()
         libscreen()
     elif optdb==5:
+        while True:
+            try:
+                bookno = int(input('Enter book serial number: '))
+                break
+            except ValueError:
+                print('Value Error! Please try again!')
+        libraryfunc.checkbookavail(bookno)
+        libscreen()
+    elif optdb==6:
         mainscreen()
 
 def recscreen():
@@ -91,29 +82,57 @@ def recscreen():
     4) BACK TO MAIN PAGE
     ''')
 
-    optrecord = int(input('Enter your choice: '))
+    while True:
+        try:
+            optrecord = int(input('Enter you choice: '))
+            break
+        except ValueError:
+            print('Value Error! Please try again')
+            recscreen()
 
     if optrecord==1:
         print('ENTER YOUR DETAILS BELOW')
         name = input('Enter your name: ')
         classs = input('Enter your class: ')
         today = date.today()
-        bookno = int(input('Enter book serial number: '))
-        libraryfunc.borrowbook(bookno)
-        recordfunc.insertrecord(name,classs,today,bookno)
-        print('BORROWED BOOK SUCCESSFULLY!')
+        while True:
+            try:
+                bookno = int(input('Enter book serial number: '))
+                break
+            except ValueError:
+                print('Value Error! Please try again!')
+        if libraryfunc.checksno(bookno) == True:
+            libraryfunc.borrowbook(bookno)
+            recordfunc.insertrecord(name,classs,today,bookno)
+            print('BORROWED BOOK SUCCESSFULLY!')
+        else:
+            print('Try again!')
         recscreen()
     elif optrecord==2:
         print('ENTER YOUR DETAILS BELOW')
         name = input('Enter your name: ')
         classs = input('Enter your class: ')
         today = date.today()
-        bookno = int(input('Ent(er book serial number: '))
-        recordfunc.insertrecord(name,classs,today,bookno)
-        libraryfunc.returnbook(bookno)
+        while True:
+            try:
+                bookno = int(input('Enter book serial number: '))
+                break
+            except ValueError:
+                print('Value Error! Please try again!')
+        if libraryfunc.checksno(bookno) == True:
+            libraryfunc.returnbook(bookno)
+            recordfunc.insertrecord(name,classs,today,bookno)
+            print('RETURNED BOOK SUCCESSFULLY!')
+        else:
+            print('Try again!')
         recscreen()
     elif optrecord==3:
-        bookno = int(input('Enter Book serial number: '))
+        while True:
+            try:
+                bookno = int(input('Enter book serial number: '))
+                break
+            except ValueError:
+                print('Value Error! Please try again!')
         libraryfunc.checkbookavail(bookno)
         recscreen()
     elif optrecord==4:
@@ -121,11 +140,16 @@ def recscreen():
 
 def abtscreen():
     print('''
+
     SCHOOL LIBRARY MANAGEMENT SYSTEM
+
     PROGRAM MADE BY STUDENTS OF KENDRIYA VIDYALAYA NO.2 :
     AVINASH
     KEVIN IMMANUEL
     SANCHIT SAINI
+
+    AND WITH THE SPECIAL GUIDANCE OF OUR COMPUTER SCIENCE TEACHER MRS.LEENA
+
     ''')
     abtopt = input('Enter to go back to main screen')
     if abtopt!='':
