670023c50c7c596020bdf84666a74e1e867f389c
Kevin Immanuel <kevin.exe4real@gmail.com>
2023-01-03 22:48:10 +0530

    kdsjnksvjns

--- createdb.py
+++ createdb.py
@@ -6,7 +6,7 @@ cur = db.cursor()
 def createdb():
     cur.execute('create database library')
     cur.execute('use library')
-    cur.execute('create table lib (snobook int primary key not null, name varchar(30) not null, author varchar(30) not null, price int not null, pages int not null, available boolean not null);')
+    cur.execute('create table lib (snobook int primary key not null, name varchar(60) not null, author varchar(60) not null, price int not null, pages int not null, available boolean not null);')
     cur.execute('create table record (sno int primary key not null, dofentry date not null, name varchar(30) not null, class varchar(10) not null, snobook int not null')
     db.commit()
 
